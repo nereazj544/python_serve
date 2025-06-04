720a05f31fe89a18452825c3aff41c27d15abffd
nereazj544 <nzjvirtual@gmail.com>
2025-06-04 16:14:32 +0200

    .

--- servidor_v2_mongodb.py
+++ servidor_v2_mongodb.py
@@ -9,17 +9,20 @@ from Logs import log_info, log_error, log_warning, log_debug, log_critical
 # Configuraciones de conexión - MongoDB
 HOST = "localhost"
 PORT = 8080
+tiempo_espera = 5  # Tiempo de espera en segundos
 
 DB = "test_python_db"
 COLLECTION_1 = "empresas"
 COLLECTION_2 = "personajes"
 COLLECTION_3 = "Lenguajes"
-tiempo_espera = 5  # Tiempo de espera en segundos
+
+DB_2 = "ZOO"
+COLLECTION_2_1 = "ANIMALES"
+
+
 client = MongoClient("mongodb://localhost:27017/")
 
 def collection_empresas(conn: socket.socket):
-    
-    
     time.sleep(tiempo_espera)  # Espera de 5 segundos antes de continuar
     log_info(f"Seleccionada la colección: {COLLECTION_1}")
     conn.send("\n¿Qué quieres hacer? 1. Insertar 2. Buscar 3. Salir".encode())
@@ -53,10 +56,38 @@ def collection_empresas(conn: socket.socket):
             log_info("Saliendo de la colección de empresas.")
             break
 
+def collection_personajes(conn: socket.socket):
+    pass
 
-
-
-
+def collection_animales(conn: socket.socket):
+    log_info(f"Seleccionada la colección: {COLLECTION_2_1}")
+    time.sleep(tiempo_espera)  # Espera de 5 segundos antes de continuar
+    conn.send("\n¿Qué quieres hacer? 1. Insertar 2. Buscar 3. Salir".encode())
+    while True:
+        msg = conn.recv(1024).decode()
+        log_info(f"Mensaje recibido: {msg}")
+        if msg == "1":
+            collection = client[DB_2][COLLECTION_2_1]
+            conn.send("\nIngrese el NOMBRE del animal a AÑADIR: ".encode())
+            nombre = conn.recv(1024).decode().capitalize()
+            data = {
+                "id": collection.count_documents({}) + 1,
+                "nombre": nombre
+            }
+            collection.insert_one(data)
+            conn.send("Animal insertado correctamente. PARA CONTINUAR 'ENTER'".encode())
+            log_info(f"Animal insertado: {data}")
+            collection_animales(conn)
+        elif msg == "2":
+            collection = client[DB_2][COLLECTION_2_1]
+            conn.send("\nIngrese el NOMBRE del animal a BUSCAR: ".encode())
+            nombre = conn.recv(1024).decode()
+            animal = collection.find_one({"nombre": nombre.capitalize()})
+            if animal:
+                conn.send(f"Animal encontrado: {nombre} número del animal: {animal['id']}\nPARA CONTINUAR 'ENTER' ".encode())
+            else:
+                conn.send("Animal no encontrado.".encode())
+            collection_animales(conn)
 
 
 # TODO: START SERVER
@@ -65,14 +96,13 @@ def start_server():
         s.bind((HOST, PORT))
         s.listen(5)
         log_info(f"Servidor escuchando en {HOST}:{PORT}")
-
         while True:
             conn, addr = s.accept()
             with conn:
                 print (f"Conexion establecida desde {addr}")
                 log_info(f"Conexión establecida desde {addr}")
 
-                conn.send("Elige una coleccion:\n1. Empresas \n2. Personajes \n3. Lenguajes \nEscribe un número: ".encode())
+                conn.send("Elige una coleccion:\n1. Empresas \n2. Personajes \n3. Lenguajes \n4. Animales\nEscribe un número: ".encode())
                 msg = conn.recv(1024).decode()
                 print(f"Mensaje recibido: {msg}")
                 log_info(f"Mensaje recibido: {msg}")
@@ -80,7 +110,6 @@ def start_server():
                 if msg == "1":
                     print(f"Coleccion seleccionada: {COLLECTION_1}")
                     conn.send(f"Coleccion seleccionada: {COLLECTION_1}\n ENTER".encode())
-                    
                     log_info(f"Coleccion seleccionada: {COLLECTION_1}")
 
                     time.sleep(tiempo_espera)
@@ -88,7 +117,20 @@ def start_server():
                 elif msg == "2":
                     print(f"Coleccion seleccionada: {COLLECTION_2}")
                     log_info(f"Coleccion seleccionada: {COLLECTION_2}")
-                    # collection_personajes(conn)
+                    conn.send(f"Colección de {COLLECTION_2} aún no implementada.\n".encode())
+                    
+                elif msg == "3":
+                    print(f"Coleccion seleccionada: {COLLECTION_3}")
+                    log_info(f"Coleccion seleccionada: {COLLECTION_3}")
+                    conn.send(f"Colección de {COLLECTION_3} aún no implementada.\n".encode())
+                elif msg == "4":
+                    print(f"Coleccion seleccionada: {COLLECTION_2_1}")
+                    log_info(f"Coleccion seleccionada: {COLLECTION_2_1}")
+                    collection_animales(conn)
+                    time.sleep(tiempo_espera)
+
+                
+
 
 
 if __name__ == "__main__":
