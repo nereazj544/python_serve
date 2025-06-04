3237fc2bd1badd87f8cd77cdb55629ce67af1f0d
nereazj544 <nzjvirtual@gmail.com>
2025-06-04 13:50:46 +0200

    Agregar funciones para manejar colecciones en el servidor MongoDB

--- servidor_v1_mongodb.py
+++ servidor_v1_mongodb.py
@@ -15,6 +15,15 @@ COLLECTION_1 = "empresas"
 COLLECTION_2 = "personajes"
 COLLECTION_3 = "Lenguajes"
 
+def COLLECTION_empresas():
+    
+    raise NotImplementedError
+
+def COLLECTION_personajes():
+    raise NotImplementedError
+
+def COLLECTION_lenguajes():
+    raise NotImplementedError
 
 
 #! SERVIDOR
@@ -34,8 +43,22 @@ def start():
             conn, addr = s.accept()
             print(f"Conexión establecida desde {addr}")
             log_info(f"Conexión establecida desde {addr}")
+            msg = conn.recv(1024).decode()
+            print(f"Mensaje recibido: {msg}")
+            log_info(f"Mensaje recibido: {msg}")
 
-
+            if msg == "1":
+                print(f"Coleccion seleccionada: {COLLECTION_1}")
+                log_info(f"Coleccion seleccionada: {COLLECTION_1}")
+                COLLECTION_empresas()
+            elif msg == "2":
+                print(f"Coleccion seleccionada: {COLLECTION_2}")
+                log_info(f"Coleccion seleccionada: {COLLECTION_2}")
+                COLLECTION_personajes()
+            elif msg == "3":
+                print(f"Coleccion seleccionada: {COLLECTION_3}")
+                log_info(f"Coleccion seleccionada: {COLLECTION_3}")
+                COLLECTION_lenguajes()
 
     except Exception as e:
         print (f"Se ha producido un error al iniciar el servidor: {e}")
