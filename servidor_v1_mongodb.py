b64b460f44cf5de677e2b0b5667b914aea91e260
nereazj544 <nzjvirtual@gmail.com>
2025-06-04 13:12:05 +0200

    Implementar servidor básico con conexión a MongoDB y manejo de errores

--- servidor_v1_mongodb.py
+++ servidor_v1_mongodb.py
@@ -1,14 +1,48 @@
 
 #! LIBRERIAS 
 from pymongo import MongoClient
+import socket
 from Logs import log_info, log_error, log_warning, log_debug, log_critical
 
 
+
+# TODO: CONFIGURACIONES DE CONEXION - MONGODB
 HOST = "localhost"
-PORT = 27017
+PORT = 8080
+
+DB = "test_python_db"
+COLLECTION_1 = "empresas"
+COLLECTION_2 = "personajes"
+COLLECTION_3 = "Lenguajes"
+
+
 
 #! SERVIDOR
 
 def start():
-    pass
+    print(". . . SERVER ON . . .")
+    log_info("Servidor iniciado correctamente.")
 
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        print(f"Servidor escuchando en {HOST}:{PORT}")
+        log_info(f"Servidor escuchando en {HOST}:{PORT}")
+
+        while True:
+            conn, addr = s.accept()
+            print(f"Conexión establecida desde {addr}")
+            log_info(f"Conexión establecida desde {addr}")
+
+
+
+    except Exception as e:
+        print (f"Se ha producido un error al iniciar el servidor: {e}")
+        log_error(f"Error al iniciar el servidor: {e}")
+    
+
+
+
+
+start()
