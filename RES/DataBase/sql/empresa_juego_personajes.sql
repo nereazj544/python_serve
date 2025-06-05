eaf0787455368af8963d29b010422088f7e1567e
nereazj544 <nzjvirtual@gmail.com>
2025-06-05 12:32:09 +0200

    Agregar campo de rareza a la tabla de personajes y actualizar datos iniciales. Mejorar la estructura del servidor con nuevas importaciones y comentarios.

--- RES/DataBase/sql/empresa_juego_personajes.sql
+++ RES/DataBase/sql/empresa_juego_personajes.sql
@@ -13,6 +13,7 @@ create table personajes(
     id int auto_increment PRIMARY KEY,
     nombre VARCHAR(255) NOT Null,
     elemento VARCHAR(255) NOT Null,
+    rareza VARCHAR(10),
     juego_id int,
     foreign key (juego_id) references juegos(id)
 );
@@ -29,22 +30,22 @@ insert INTO juegos (nombre, genero, empresa_id) values
 
 
 
-insert INTO personajes (nombre, elemento, juego_id) values
-('Bennett', 'Pyro',1),
-('Kazuha','Anemo', 1),
-('Arlecchino', 'Pyro' ,1),
-('Xiao', 'Anemo', 1),
-('Jing Yan', 'Rayo', 2),
-('Kafka', 'Rayo', 2),
-('Gepard', 'Hielo', 2),
-('Topaz y Conti', 'Fuego', 2),
-('Jiyan', 'Aero', 3),
-('Calcharo', 'Electro', 3),
-('Lingyang', 'Glacio', 3),
-('Encore', 'Fusion', 3),
-('Arion', 'Aire', 4),
-('Mark', 'Montaña', 4),
-('Umei', 'Bosque', 4),
-('Sonny', 'Fuego', 4),
-('Lycaon', 'Hielo', 5),
-('Lighter', 'Fuego', 5);
+INSERT INTO personajes (nombre, elemento, rareza, juego_id) VALUES
+('Bennett', 'Pyro', '4', 1),
+('Kazuha', 'Anemo', '5', 1),
+('Arlecchino', 'Pyro', '5', 1),
+('Xiao', 'Anemo', '5', 1),
+('Jing Yan', 'Rayo', '5', 2),
+('Kafka', 'Rayo', '5', 2),
+('Gepard', 'Hielo', '5', 2),
+('Topaz y Conti', 'Fuego', '5', 2),
+('Jiyan', 'Aero', '5', 3),
+('Calcharo', 'Electro', '5', 3),
+('Lingyang', 'Glacio', '5', 3),
+('Encore', 'Fusion', '5', 3),
+('Arion', 'Aire', NULL, 4),
+('Mark', 'Montaña', NULL, 4),
+('Umei', 'Bosque', NULL, 4),
+('Sonny', 'Fuego', NULL, 4),
+('Lycaon', 'Hielo', '5', 5),
+('Lighter', 'Fuego', '5', 5);
--- server_max/servidor_V1_Masivo.py
+++ server_max/servidor_V1_Masivo.py
@@ -1,10 +1,28 @@
 ''' 
 SERVIDOR V1_Masivo.py
 
-Este servidor lo que hace es que va a realizar son consultas a dos bases de datos diferentes (SQL y MongoDB). Donde el cliente sera el encargado de enviar las 'peticiones' y el servidor las procesara.
+Este servidor lo que hace es que va a realizar son consultas a dos bases de datos diferentes (SQL y MongoDB). 
+
+Donde el cliente sera el encargado de enviar las 'peticiones' y el servidor las procesara.
+
 En esta versión, el servidor sera simple (no asincrono (eso sera en la versión 2)) y se encargara de procesar las peticiones de forma secuencial.
 
 '''
 
 
-#! IMPORTACIONES
+#TODO: Librerias
+    # ? SOCKETS
+import socket  # Configuración del servidor y cliente
+import time  # Para manejar tiempos de espera y pausas
+
+    #! BASES DE DATOS
+from pymongo import MongoClient  # Para manejar la base de datos MongoDB
+import mysql.connector  # Para manejar la base de datos MySQL
+
+    # ? LOGS 
+from Logs import log_info, log_error, log_warning, log_debug, log_critical  # Para manejar los logs
+
+
+# * ==========================================================
+
+
