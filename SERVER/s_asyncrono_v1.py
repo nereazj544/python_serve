076c72fa8f5ebb46f407520754130d5cc6c3bb18
nereazj544 <nzjvirtual@gmail.com>
2025-06-09 15:46:32 +0200

    Agregar opción para consultar personajes por rareza en la colección 'Personajes' de MongoDB.

--- SERVER/s_asyncrono_v1.py
+++ SERVER/s_asyncrono_v1.py
@@ -263,7 +263,26 @@ async def collection_MDB_4(writer, reader):
                 else:
                     writer.write("No se encontró ningún personaje con ese arma.\n".encode())
 
+            elif msg == "6":
+                writer.write("RAREZA PERSONAJE: ".encode())
+                await writer.drain()
+                rareza_personaje = (await reader.read(1024)).decode().strip().capitalize()
+                col = COL_P.find({"rareza": rareza_personaje})
+                if col:
+                    lista = f"Personajes encontrados con la rareza {rareza_personaje}:\n"
+                    for c in col:
+                        lista += f"ID: {c.get('id', 'Desconocido')} | Nombre: {c.get('nombre', 'Desconocido')} | Rareza: {c.get('rareza', 'Desconocida')} | Arma: {c.get('arma', 'Desconocida')} | Elemento: {c.get('elemento', 'Desconocido')} | Facción: {c.get('faccion', 'Desconocida')}\n"
+                    writer.write(f"{lista}".encode())
+                    lista = COL_P.count_documents({"rareza": rareza_personaje})
+                    writer.write(f"Total de personajes encontrados con la rareza {rareza_personaje}: {lista}\n".encode())
 
+                else:
+                    writer.write("No se encontró ningún personaje con esa rareza.\n".encode())
+
+            elif msg == "7":
+                writer.write("Volviendo al menú principal.\n".encode())
+                await writer.drain()
+                break
 
 
 
