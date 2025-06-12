655006adab27260837f285d42b71d0bfb50721ad
nereazj544 <nzjvirtual@gmail.com>
2025-06-12 14:15:32 +0200

    Agregar funcionalidades para agregar y eliminar animales, incluyendo validaciones y mensajes de éxito/error en la API.

--- API/API_mongodb.py
+++ API/API_mongodb.py
@@ -19,6 +19,26 @@ def home():
         item['_id'] = str(item['_id'])
     return render_template('index.html', items=items)
 
+@app.route('/add', methods=['POST'])
+def add_item():
+    name = request.form.get('nombre')
+    species = request.form.get('especie')
+
+    if name and species:
+        collection.insert_one({
+            'id': collection.count_documents({}) + 1,
+            'nombre': name, 'especie': species})
+        return jsonify({'status': 'success', 'message': '¡Elemento agregado exitosamente!'})
+    else:
+        return jsonify({'status': 'error', 'message': '¡Todos los campos son obligatorios!'}), 400
+    
+@app.route('/delete', methods=['POST'])
+def delete_item(item_id):
+    result = collection.delete_one({'id': int(item_id)})
+    if result.deleted_count > 0:
+        return jsonify({'status': 'success', 'message': '¡Elemento eliminado exitosamente!'})
+    else:
+        return jsonify({'status': 'error', 'message': '¡Elemento no encontrado!'}), 404
 
 
 if __name__ == '__main__':
