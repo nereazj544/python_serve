20bd323ff89e25dd3bb08eb6414921c0611a44c5
nereazj544 <nzjvirtual@gmail.com>
2025-06-13 09:38:06 +0200

    Agregar estructura básica de la aplicación Flask y ruta inicial para la página de inicio.

--- API/API/API_v1.py
+++ API/API/API_v1.py
@@ -2,3 +2,23 @@ from flask import Flask, jsonify, request, render_template
 
 from pymongo import MongoClient
 
+# TODO: ======= CONFIGURACION DE LA BASE DE DATOS =======
+client = MongoClient('mongodb://localhost:27017/')
+db = client
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
