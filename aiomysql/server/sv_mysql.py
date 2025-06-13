c12bd1d2d7392c7b1912eca3fe122bfb207d08ca
nereazj544 <nzjvirtual@gmail.com>
2025-06-13 11:38:36 +0200

    Agregar funciones para la gestión de animales en la base de datos y mejorar el registro de eventos del servidor.

--- aiomysql/server/sv_mysql.py
+++ aiomysql/server/sv_mysql.py
@@ -3,7 +3,8 @@ import aiomysql
 from Logs import log_critical, log_info, log_error, log_warning, log_debug
 from aiomysql import create_pool
 
-
+Shost = 'localhost'  # Dirección del servidor
+Sport = 8083  # Puerto del servidor
 
 #todo: configuracion de la base de datos
 MySQL_HOST = 'localhost'
@@ -19,3 +20,212 @@ async def connect():
         db=MySQL_DB
     )
     return conn
+
+
+
+
+
+async def animales_mysql(reader, writer):
+    conn = await connect()
+    cursor = await conn.cursor()
+
+    log_info("Conexión establecida con la base de datos MySQL para Animales.")
+    print("Conexión establecida con la base de datos MySQL para Animales.")
+    writer.write("Conexión establecida con la base de datos MySQL para Animales.\n".encode())
+    await writer.drain()
+
+    while True:
+        menu = ("1. Ver todos los animales\n"
+        "2. Filtar animales por especie\n"
+        "3. salir")
+        writer.write(menu.encode())
+        await writer.drain()
+        data = await reader.read(1024)
+        msg = data.decode().strip()
+        log_debug(f"Opción seleccionada: {msg}")
+        if msg == '1':
+            log_info("Opción Ver todos los animales seleccionada.")
+            writer.write("Has seleccionado la opción Ver todos los animales.\n".encode())
+            await writer.drain()
+            await cursor.execute("SELECT * FROM animales")
+            rows = await cursor.fetchall()
+            if rows:
+                for row in rows:
+                    writer.write(f"\nID: {row[0]}, Nombre: {row[1]}, Especie: {row[2]}\n".encode())
+            await writer.drain()
+        elif msg == '2':
+            log_info("Opción Filtrar animales por especie seleccionada.")
+            print("Opción Filtrar animales por especie seleccionada.")
+            writer.write("Has seleccionado la opción Filtrar animales por especie.\n".encode())
+            await writer.drain()
+            writer.write("Introduce la especie a filtrar: ".encode())
+            await writer.drain()
+            data = await reader.read(1024)
+            especie = data.decode().strip()
+            if not especie:
+                log_warning("Especie no válida.")
+                print("Especie no válida.")
+                writer.write("Especie no válida.\n".encode())
+                await writer.drain()
+                continue
+            log_debug(f"Especie a filtrar: {especie}")
+
+
+            lista_especie = 'LISTA DE ANIMALES FILTRADOS POR ESPECIE:\n'
+            await cursor.execute(f"select a.nombre, a.especie, rc.nombre, rc.id from animales a inner join numero_recinto nr on a.id = nr.animal_id inner join recintos rc on nr.recinto_id = rc.id where a.especie like '%{especie}%'")
+            rows = await cursor.fetchall()
+            if rows:
+                for row in rows:
+                    lista_especie +=(f"Nombre: {row[0]} | Especie: {row[1]} | Recinto: {row[2]} | Numero recinto: {row[3]} \n")
+                writer.write(lista_especie.encode())
+            await writer.drain()
+            await server_on(reader, writer)  # Volver al menú principal
+
+def recintos_mysql(reader, writer):
+    pass
+
+def cuidador_mysql(reader, writer):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO: ============== SERVIDOR ==============
+
+async def server_on(reader, writer):  
+    addr = writer.get_extra_info('peername')
+    log_info(f"Conexión desde {addr}")
+    print(f"Conexión desde {addr}")
+    writer.write("CONEXIÓN ESTABLECIDA CORRECTAMENTE CON EL SERVIDOR.\n ENTER PARA CONTINUAR".encode())
+    await writer.drain()
+
+    while True:
+        await asyncio.sleep(5)
+        log_warning("== ESPERA AUTOMÁTICA DE 5 SEGUNDOS ==")
+        await writer.drain()
+        writer.write("ELIGE UNA OPCIÓN:\n" \
+        "1. Animales\n" \
+        "2. Recintos\n" \
+        "3. Cuidador\n" \
+        "4. Salir".encode())
+        await writer.drain()
+        msg = await reader.read(1024)
+        if not msg:
+            log_warning("Cliente desconectado.")
+            print("Cliente desconectado.")
+            break
+        option = msg.decode().strip()
+        log_debug(f"Opción seleccionada: {option}")
+        print(f"Opción seleccionada: {option}")
+        
+        if option == '1':
+            log_info("Opción Teleoperador seleccionada.")
+            writer.write("Has seleccionado la opción Animales.\n".encode())
+            await writer.drain()
+            await animales_mysql(reader, writer)
+        elif option == '2':
+            log_info("Opción Recintos seleccionada.")
+            writer.write("Has seleccionado la opción Recintos.\n".encode())
+            await writer.drain()
+            await recintos_mysql(reader, writer)
+        elif option == '3':
+            log_info("Opción Cuidador seleccionada.")
+            writer.write("Has seleccionado la opción Cuidador.\n".encode())
+            await writer.drain()
+            await cuidador_mysql(reader, writer)
+        elif option == '4':
+            log_info("Opción Salir seleccionada.")
+            writer.write("Saliendo del servidor...\n".encode())
+            await writer.drain()
+            log_critical("Servidor cerrado por el usuario.")
+            print("Servidor cerrado por el usuario.")
+            break
+
+        else:
+            log_warning("Opción no válida, por favor intente nuevamente.")
+            writer.write("Opción no válida, por favor intente nuevamente.\n".encode())
+        await writer.drain()
+
+
+
+async def main ():
+    try:
+            server = await asyncio.start_server(
+            server_on, Shost, Sport
+        )
+            log_info(f"Servidor escuchando en {Shost}:{Sport}")
+            print(f"Servidor escuchando en {Shost}:{Sport}")
+            async with server:
+                await server.serve_forever()
+    except Exception as e:
+        log_error(f"Error al iniciar el servidor: {e}")
+        print(f"Error al iniciar el servidor: {e}")
+    except KeyboardInterrupt:
+        log_warning("Servidor detenido por el usuario.")
+        print("Servidor detenido por el usuario.")
+    finally:
+        log_critical("Servidor cerrado.")
+        print("Servidor cerrado.")
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
