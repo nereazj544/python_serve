9ee3b2f1b0c8bc15266e636754087ec38d194426
nereazj544 <nzjvirtual@gmail.com>
2025-06-13 12:02:48 +0200

    Agregar funcionalidad para insertar un nuevo animal y asignarlo a un recinto en la gestión de animales.

--- aiomysql/server/sv_mysql.py
+++ aiomysql/server/sv_mysql.py
@@ -22,9 +22,6 @@ async def connect():
     return conn
 
 
-
-
-
 async def animales_mysql(reader, writer):
     conn = await connect()
     cursor = await conn.cursor()
@@ -49,11 +46,14 @@ async def animales_mysql(reader, writer):
             writer.write("Has seleccionado la opción Ver todos los animales.\n".encode())
             await writer.drain()
             await cursor.execute("SELECT * FROM animales")
+            
             rows = await cursor.fetchall()
+            
             if rows:
                 for row in rows:
                     writer.write(f"\nID: {row[0]}, Nombre: {row[1]}, Especie: {row[2]}\n".encode())
             await writer.drain()
+        
         elif msg == '2':
             log_info("Opción Filtrar animales por especie seleccionada.")
             print("Opción Filtrar animales por especie seleccionada.")
@@ -116,6 +116,130 @@ async def animales_mysql(reader, writer):
                 log_info(f"Animal {nombre} de especie {especie} insertado correctamente.")
                 print(f"Animal {nombre} de especie {especie} insertado correctamente.")
                 writer.write(f"Animal {nombre} de especie {especie} insertado correctamente.\n".encode())
+                
+                
+                writer.write("Lista de animales y recintos disponibles:\n".encode())
+                
+                lista_animales = 'LISTA DE ANIMALES:\n'
+                await cursor.execute("SELECT id, nombre FROM animales")
+                rows = await cursor.fetchall()
+                if rows:
+                    for row in rows:
+                        lista_animales += f"ID: {row[0]}, Nombre: {row[1]}\n"
+                    writer.write(lista_animales.encode())
+                await writer.drain()
+                writer.write("Introduce el ID del animal: ".encode())
+                await writer.drain()
+                data = await reader.read(1024)
+                animal_id = data.decode().strip()
+                if not animal_id.isdigit():
+                    log_warning("ID de animal no válido.")
+                    print("ID de animal no válido.")
+                    writer.write("ID de animal no válido.\n".encode())
+                    await writer.drain()
+                    continue
+                log_debug(f"ID del animal: {animal_id}")
+
+
+                lista_recintos = 'LISTA DE RECINTOS:\n'
+                await cursor.execute("SELECT id, nombre FROM recintos")
+                rows = await cursor.fetchall()
+                if rows:
+                    for row in rows:
+                        lista_recintos += f"ID: {row[0]}, Nombre: {row[1]}\n"
+                    writer.write(lista_recintos.encode())
+            
+                await writer.drain()
+                writer.write("Hay un recinto para el animal seleccionado? (s/n)\n".encode())
+                await writer.drain()
+                data = await reader.read(1024)
+                respuesta = data.decode().strip().lower()
+                if respuesta == 's' or respuesta == 'si' or respuesta == 'yes' or respuesta == 'y':
+                    log_info("Respuesta afirmativa")
+                    
+                    writer.write("Introduce el ID del recinto donde se ubicará el animal: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    recinto_id = data.decode().strip()
+                    log_info(f"ID del recinto: {recinto_id}")
+
+                    query = "INSERT INTO numero_recinto (animal_id, recinto_id) VALUES (%s, %s)"
+                    await cursor.execute(query, (animal_id, recinto_id))
+                    await conn.commit()
+                    log_info(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.")
+                    print(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.")
+                    writer.write(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.\n".encode())
+                
+                elif respuesta == 'n' or respuesta == 'no':
+                    log_info("Respuesta negativa")
+                    writer.write("Crear recinto para el animal.\n".encode())
+                    await writer.drain()
+
+                    writer.write("Introduce el nombre del recinto: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    recinto_nombre = data.decode().strip().capitalize()
+                    log_info(f"Nombre del recinto: {recinto_nombre}")
+
+                    writer.write("Introduce la ubicación del recinto: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    recinto_ubicacion = data.decode().strip().capitalize()
+                    log_info(f"Ubicación del recinto: {recinto_ubicacion}")
+
+                    writer.write("Introduce la capacidad del recinto: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    recinto_capacidad = data.decode().strip()
+                    log_info(f"Capacidad del recinto: {recinto_capacidad}")
+
+                    query = "INSERT INTO recintos (nombre, ubicacion, capacidad) VALUES (%s, %s, %s)"
+                    await cursor.execute(query, (recinto_nombre, recinto_ubicacion, recinto_capacidad))
+                    await conn.commit()
+                    log_info(f"Recinto {recinto_nombre} creado correctamente.")
+                    
+                    writer.write(f"Recinto {recinto_nombre} creado correctamente.\n".encode())
+                    await writer.drain()
+
+                    writer.write("Lista de recintos actualizada.\n".encode())
+                    lista_animales = 'LISTA DE ANIMALES:\n'
+                    await cursor.execute("SELECT id, nombre FROM animales")
+                    rows = await cursor.fetchall()
+                    if rows:
+                        for row in rows:
+                            lista_animales += f"ID: {row[0]}, Nombre: {row[1]}\n"
+                        writer.write(lista_animales.encode())
+                    await writer.drain()
+                    writer.write("Introduce el ID del animal: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    animal_id = data.decode().strip()
+                    if not animal_id.isdigit():
+                        log_warning("ID de animal no válido.")
+                        print("ID de animal no válido.")
+                        writer.write("ID de animal no válido.\n".encode())
+                        await writer.drain()
+                        continue
+                    log_debug(f"ID del animal: {animal_id}")
+    
+                    writer.write("Introduce el ID del recinto donde se ubicará el animal: ".encode())
+                    await writer.drain()
+                    data = await reader.read(1024)
+                    recinto_id = data.decode().strip()
+                    log_info(f"ID del recinto: {recinto_id}")
+    
+                    query = "INSERT INTO numero_recinto (animal_id, recinto_id) VALUES (%s, %s)"
+                    await cursor.execute(query, (animal_id, recinto_id))
+                    await conn.commit()
+                    log_info(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.")
+                    print(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.")
+                    writer.write(f"Animal {animal_id} asignado al recinto {recinto_id} correctamente.\n".encode())
+                    await writer.drain()
+
+
+
+
+
 
                 
             except Exception as e:
@@ -135,32 +259,6 @@ async def animales_mysql(reader, writer):
 
 
 
-
-async def recintos_mysql(reader, writer):
-    log_info("Conexión establecida con la base de datos MySQL para Recintos.")
-    print("Conexión establecida con la base de datos MySQL para Recintos.")
-    writer.write("Conexión establecida con la base de datos MySQL para Recintos.\n".encode())
-    await writer.drain()
-    while True:
-        menu = ("1. Ver todos los recintos\n"
-        "2. Filtrar recintos por nombre\n"
-        "3. salir")
-        writer.write(menu.encode())
-        await writer.drain()
-        data = await reader.read(1024)
-        msg = data.decode().strip()
-
-
-
-
-
-
-
-async def cuidador_mysql(reader, writer):
-    log_info("Conexión establecida con la base de datos MySQL para Cuidadores.")
-    print("Conexión establecida con la base de datos MySQL para Cuidadores.")
-    writer.write("Conexión establecida con la base de datos MySQL para Cuidadores.\n".encode())
-    await writer.drain()
 
 
 
@@ -228,10 +326,8 @@ async def server_on(reader, writer):
         log_warning("== ESPERA AUTOMÁTICA DE 5 SEGUNDOS ==")
         await writer.drain()
         writer.write("ELIGE UNA OPCIÓN:\n" \
-        "1. Animales\n" \
-        "2. Recintos\n" \
-        "3. Cuidador\n" \
-        "4. Salir".encode())
+        "1. Inicio\n" \
+        "2. Salir\n".encode())
         await writer.drain()
         msg = await reader.read(1024)
         if not msg:
@@ -244,20 +340,9 @@ async def server_on(reader, writer):
         
         if option == '1':
             log_info("Opción Teleoperador seleccionada.")
-            writer.write("Has seleccionado la opción Animales.\n".encode())
             await writer.drain()
             await animales_mysql(reader, writer)
         elif option == '2':
-            log_info("Opción Recintos seleccionada.")
-            writer.write("Has seleccionado la opción Recintos.\n".encode())
-            await writer.drain()
-            await recintos_mysql(reader, writer)
-        elif option == '3':
-            log_info("Opción Cuidador seleccionada.")
-            writer.write("Has seleccionado la opción Cuidador.\n".encode())
-            await writer.drain()
-            await cuidador_mysql(reader, writer)
-        elif option == '4':
             log_info("Opción Salir seleccionada.")
             writer.write("Saliendo del servidor...\n".encode())
             await writer.drain()
@@ -269,7 +354,6 @@ async def server_on(reader, writer):
             log_warning("Opción no válida, por favor intente nuevamente.")
             writer.write("Opción no válida, por favor intente nuevamente.\n".encode())
         await writer.drain()
-
 
 
 async def main ():
