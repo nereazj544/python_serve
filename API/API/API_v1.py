c46c5889f908ea6f2c740db9b12ef6cca7c8c52e
nereazj544 <nzjvirtual@gmail.com>
2025-06-13 14:45:35 +0200

    Actualizar la configuración de la base de datos y agregar funcionalidades para añadir y eliminar animales. Mejorar la estructura del formulario y la presentación de la tabla en la interfaz.

--- API/API/API_v1.py
+++ API/API/API_v1.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 
 # TODO: ======= CONFIGURACION DE LA BASE DE DATOS =======
 client = MongoClient('mongodb://localhost:27017/')
-db = client['ZOO']
-collection = db['ANIMALES']
+db = client['zoo3']
+collection = db['animales']
 
 
 
@@ -12,12 +12,55 @@ collection = db['ANIMALES']
 
 app = Flask(__name__)
 
+# TODO : ======= RUTAS DE LA APLICACION =======
 @app.route('/')
-def home():
+def home(): # ? Ruta principa
     items = list(collection.find())
     for item in items:
         item['_id'] = str(item['_id'])
     return render_template('index.html', items=items)
+
+
+#TODO: AGREGAR UN ANIMAL
+@app.route('/add', methods=['POST'])
+def add_animal():
+    nombre = request.form.get('nombre').capitalize()
+    entorno = request.form.get('entorno').capitalize()
+    tipo_reproduccion = request.form.get('tipo_reproduccion').capitalize()
+    if nombre and entorno and tipo_reproduccion:
+        collection.insert_one({
+            'id': collection.count_documents({}) +1, # Genera un ID aumentando uno al anterior
+            'nombre': nombre,
+            'entorno': entorno,
+            'tipo_reproduccion': tipo_reproduccion
+        })        
+        return jsonify({'status': 'succes', 'message': '¡Animal agregado exitosamente!'})
+    else:
+        return jsonify({'status': 'error', 'message': '¡Todos los campos son obligatorios!'}), 400
+    
+#TODO: ELIMINAR UN ANIMAL
+@app.route('/delete', methods=['POST'])
+def delete_animal():
+    try:
+        item_id = int(request.form.get('id'))
+        if not item_id:
+            return jsonify({'status': 'error', 'message': '¡ERROR: ID NO VALIDO!'}), 400
+        
+        item_id = int(item_id)
+        result = collection.delete_one({'id': item_id})
+        if result.deleted_count > 0:
+            return jsonify({'status': 'success', 'message': '¡ID ELIMINADO EXITOSAMENTE!'})
+        else:
+            return jsonify({'status': 'error', 'message': '¡ERROR: ID NO ENCONTRADO!'})
+    except:
+        return jsonify({'status': 'error', 'message': '¡ERROR AL ELIMINAR EL ELEMNTO!'})
+
+
+
+
+
+
+
 
 
 
