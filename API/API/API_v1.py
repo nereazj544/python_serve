b22b7b5422157012aea51e36f24787a62fb61ddb
nereazj544 <nzjvirtual@gmail.com>
2025-06-17 10:34:55 +0200

    Agregar documentación de las rutas para agregar un nuevo animal y la ruta principal en la API

--- API/API/API_v1.py
+++ API/API/API_v1.py
@@ -13,9 +13,24 @@ collection = db['animales']
 app = Flask(__name__)
 swagger = Swagger(app)  # Inicializar Swagger para la documentacion de la API
 
+
+
+
+
+
 # TODO : ======= RUTAS DE LA APLICACION =======
 @app.route('/')
 def home(): # ? Ruta principa
+    """
+    Ruta principal donde se muestran todos los datos de la base de datos: MongoDB
+    ---
+    responses:
+        200:
+            description: "Lista de animales"
+        500:
+            description: "Error al obtener los datos de la base de datos"
+    """
+
     items = list(collection.find())
     for item in items:
         item['_id'] = str(item['_id'])
@@ -25,6 +40,56 @@ def home(): # ? Ruta principa
 #TODO: AGREGAR UN ANIMAL
 @app.route('/add', methods=['POST'])
 def add_animal():
+
+    """
+    Ruta para agregar un nuevo animal a la base de datos
+    ---
+    parameters:
+        - name: nombre
+          in: formData
+          type: string
+          required: true
+          description: "Nombre del animal"
+        - name: entorno
+          in: formData
+          type: string
+          required: true
+          description: "Entorno del animal (terrestre, acuático, aéreo)"
+        - name: tipo_reproduccion
+          in: formData
+          type: string
+          required: true
+          description: "Tipo de reproducción del animal (ovíparo, mamífero)"
+        - name: habitat
+          in: formData
+          type: string
+          required: false
+          description: "Hábitat del animal"
+        - name: velocidad
+          in: formData
+          type: string
+          required: false
+          description: "Velocidad del animal"
+        - name: alimentacion
+          in: formData
+          type: string
+          required: false
+          description: "Tipo de alimentación del animal"
+        - name: informacion_basica
+          in: formData
+          type: string
+          required: false
+          description: "Información básica sobre el animal"
+        - name: img_url
+          in: formData
+          type: string
+          required: false
+          description: "URL de la imagen del animal"
+
+    
+    """
+
+
     nombre = request.form.get('nombre').capitalize()
     entorno = request.form.get('entorno').capitalize()
     tipo_reproduccion = request.form.get('tipo_reproduccion').capitalize()
