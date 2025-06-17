eb1584961fc71849db36f5bb904a173003661641
nereazj544 <nzjvirtual@gmail.com>
2025-06-17 13:32:49 +0200

    Agregar estilos CSS y estructura HTML para la visualización de animales en la interfaz

---
+++ API/API_2/terminal_API.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify
+from flasgger import Swagger # Swagger para documentacion de la API
+from pymongo import MongoClient
+
+
+# TODO: ======= CONFIGURACION DE LA BASE DE DATOS =======
+client = MongoClient('mongodb://localhost:27017/')
+db = client['ns']
+collection = db['terminales']
+
+app = Flask(__name__)
+swagger = Swagger(app)  # Inicializar Swagger para la documentacion de la API
+
